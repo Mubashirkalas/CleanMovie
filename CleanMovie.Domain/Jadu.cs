e4c71b96baa956350dd3ed8bd95af28337433f90
Mubashirkalas <119796720+Mubashirkalas@users.noreply.github.com>
2024-08-21 16:49:14 +0500

    16:48

---
+++ CleanMovie.Domain/Jadu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Domain
+{
+    internal class Jadu
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+      
+    }
+}
