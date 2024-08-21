dd67f8374b274beab630b97bc8f395235bfa82a2
Mubashirkalas <119796720+Mubashirkalas@users.noreply.github.com>
2024-08-21 16:19:52 +0500

    hullo

---
+++ CleanMovie.Domain/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Domain
+{
+    public class TestClass
+    {
+        public string SayHello()
+        {
+            return "Hello, CleanMovie!";
+        }
+    }
+}
